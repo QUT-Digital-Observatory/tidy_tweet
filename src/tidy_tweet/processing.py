5ae45af2bab21800ce8aa447c53e96c5bdceaf59
Sam Hames <sam.hames@qut.edu.au>
2022-02-23 11:29:52 +1000

    Don't fail when certain subobjects are missing

--- src/tidy_tweet/processing.py
+++ src/tidy_tweet/processing.py
@@ -34,15 +34,17 @@ def _load_page_object(page_json: Mapping, connection: sqlite3.Connection):
     if "media" in page_json["includes"]:
         add_mappings(mappings, mapping.map_media(page_json["includes"]["media"]))
 
-    for user in page_json["includes"]["users"]:
+    for user in page_json["includes"].get("users", []):
         add_mappings(mappings, mapping.map_user(user))
 
-    for tweet in page_json["includes"]["tweets"]:
+    for tweet in page_json["includes"].get("tweets", []):
         add_mappings(mappings, mapping.map_tweet(tweet, False))
 
     # Data
     logger.debug("Processing data section of page")
-    for tweet in page_json["data"]:
+    # Some endpoints will return responses without data (for example if all of
+    # the tweets in a hydration call are no longer available)
+    for tweet in page_json.get("data", []):
         add_mappings(mappings, mapping.map_tweet(tweet, True))
 
     logger.debug(f"About to write to {len(mappings)} tables")
