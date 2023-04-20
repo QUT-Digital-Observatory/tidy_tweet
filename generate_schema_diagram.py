5a78adce978ecc1868543708fe31f7c7ae15df1a
Elizabeth Alpert <elizabeth.alpert@qut.edu.au>
2023-04-20 12:26:13 +1000

    Add little script for schema diagram generation

---
+++ generate_schema_diagram.py
@@ -0,0 +1,200 @@
+import click
+
+from tidy_tweet.tweet_mapping import create_table_statements
+from typing import NamedTuple, List, Tuple, TextIO
+
+
+class Column(NamedTuple):
+    name: str
+    type: str
+    constraints: str = ""
+    comment: str = ""
+
+
+class ForeignKey(NamedTuple):
+    table: str
+    column: str
+    ref_table: str
+    ref_column: str
+
+
+def parse_table(statement: str) -> Tuple[str, List[Column]]:
+    """
+    Parses the create table statements in tidy_tweet.tweet mapping to extract
+    the database schema. This parser relies very much on the conventions used
+    in the tweet_mapping.py file to write the create table statements - if the
+    conventions are changed, this parser will need to be changed.
+
+    An alternative approach would be to create a sample database and then use
+    PRAGMA statements to pull out the schema, however this would not retrieve
+    the comments in the create table statements which are currently where
+    column descriptions are kept.
+    """
+    lines = [l.strip() for l in statement.splitlines()]
+
+    # Skip any blank lines at the start
+    line = lines.pop(0).strip()
+    while line == "":
+        line = lines.pop(0).strip()
+
+    # first line will be "create table ___ ("
+    table_name = line.split()[2]
+
+    columns = []
+
+    for line in lines:
+        if line == "" or line == ")" or line == ");":
+            continue
+
+        column_def, _, comment = line.partition("--")
+        # if there is no "--", the whole line will be in column_def
+
+        column_def = column_def.strip().rstrip(",").split()
+        comment = comment.strip()
+
+        if len(column_def) > 1:
+            # This line has a column definition
+            columns.append(
+                Column(
+                    name=column_def[0],
+                    type=column_def[1],
+                    constraints=" ".join(column_def[2:]),
+                    comment=comment
+                )
+            )
+        elif len(comment) > 0:
+            # This line has a comment that continues from the previous line
+            prev_column = columns.pop(len(columns) - 1)
+            comment = prev_column.comment + " " + comment
+            prev_column = prev_column._replace(comment=comment)
+            columns.append(prev_column)
+
+    return table_name, columns
+
+
+def write_table_as_list(output: TextIO, table_name: str, columns: List[Column]):
+    """
+    Prints a markdown-formatted list of the table columns
+    """
+    output.write(f"Table **{table_name}**:\n")
+
+    for col in columns:
+        col_constraints = (" " + col.constraints) if col.constraints else ""
+        col_comment = (": " + col.comment) if col.comment else ""
+
+        output.write(f"- **{col.name}** ({col.type}{col_constraints}){col_comment}\n")
+
+
+def write_schema_as_lists(output:TextIO):
+
+    for statement in create_table_statements:
+        parsed_name, parsed_columns = parse_table(statement)
+
+        write_table_as_list(output, parsed_name, parsed_columns)
+        output.write("\n")
+
+
+def write_schema_as_mermaid_er(output: TextIO, with_comments=True):
+    """
+    Generates a mermaid.js ER diagram of the database schema, formatted within
+    a markdown block
+    """
+    output.write("```mermaid\n")
+    output.write("erDiagram\n")
+
+    indent = "    "
+
+    foreign_keys: List[ForeignKey] = []
+
+    # Table definitions
+    for statement in create_table_statements:
+        table_name, columns = parse_table(statement)
+
+        output.write(indent + f'"{table_name}"' + " {\n")
+
+        for col in columns:
+            output.write(indent*2 + col.type + " " + col.name)
+
+            constraints = col.constraints
+
+            if "primary key" in constraints:
+                output.write(" PK")
+                constraints = constraints.replace("primary key", "")
+
+                # if this column is both PK & FK there needs to be a comma
+                if "references" in constraints:
+                    output.write(",")
+
+            if "references" in constraints:
+                output.write(" FK")
+
+                # need to identify the whole "references table_name (column_name)"
+                ref_start = constraints.find("references")
+                ref_end = constraints.find(")", ref_start)
+
+                _, ref_table, ref_col = constraints[ref_start:ref_end+1].split()
+                ref_col = ref_col.strip("()")
+
+                foreign_keys.append(ForeignKey(table_name, col.name, ref_table, ref_col))
+
+                # remove the expression now we've dealt with it
+                constraints = constraints[:ref_start] + constraints[ref_end+1:]
+                # Get rid of any awkward double spaces in the middle
+                constraints.replace("  ", " ")
+
+            # Extra constraints go in the comment with any actual comment
+
+            # Mermaid requires double quotes for comments
+            if (constraints or col.comment) and with_comments:
+                output.write(' "')
+
+                output.write(constraints)
+
+                if constraints and col.comment:
+                    output.write(" ")
+
+                # Can't have any double quotes inside the double quotes!
+                comment = col.comment.replace('"', "'")
+                output.write(comment)
+
+                output.write('"')
+
+            output.write("\n")
+
+        output.write(indent + "}\n")
+
+    # Relationships
+    for table, column, ref_table, ref_col in foreign_keys:
+        # This is super simple and assumes all relationships are one:many
+
+        description = column.removesuffix("_id").replace("_", " ")
+
+        output.write(indent)
+        output.write(table + " |o--o{ " + ref_table + " : " + f'"{description}"')
+        output.write("\n")
+
+    output.write("```\n\n")
+
+
+@click.command()
+@click.argument('output_file', type=click.File('w'))
+@click.option('--diagram/--skip_diagram', default=True, help="Include (default) or skip a MermaidJS ER diagram of the schema")
+@click.option('--table_list/--skip_table_list', default=True, help="Include (default) or skip a series of text lists of tables and their columns")
+@click.option('--diagram_comments/--skip_diagram_comments', default=False, help="Include or skip (default) SQL comments from the MermaidJS diagram (for readability)",)
+def write_schema_docs(output_file, diagram, table_list, diagram_comments):
+    output_file.write("# tidy_tweet database schema\n\n")
+
+    output_file.write(
+        "This is an automatically generated document describing the tables and columns "
+        "in the tidy_tweet database.\n\n"
+    )
+
+    if diagram:
+        write_schema_as_mermaid_er(output_file, with_comments=diagram_comments)
+
+    if table_list:
+        write_schema_as_lists(output_file)
+
+
+if __name__ == "__main__":
+    write_schema_docs()
---
+++ schema.md
@@ -0,0 +1,149 @@
+# tidy_tweet database schema
+
+This is an automatically generated document describing the tables and columns in the tidy_tweet database.
+
+```mermaid
+erDiagram
+    "url" {
+        text source_id
+        text source_type
+        text field
+        text url
+        text expanded_url
+        text display_url
+    }
+    "hashtag" {
+        text source_id
+        text source_type
+        text field
+        text tag
+    }
+    "mention" {
+        text source_id
+        text source_type
+        text field
+        text username
+    }
+    "media" {
+        text url
+        text preview_image_url
+        integer height
+        integer width
+        text type
+        integer duration_ms
+        integer view_count
+        string alt_text
+        text media_key PK
+    }
+    "user" {
+        text name
+        text profile_image_url
+        text id PK
+        text created_at
+        text protected
+        text description
+        text location
+        text pinned_tweet_id
+        integer verified
+        text url
+        text username
+    }
+    "tweet" {
+        text id PK
+        text reply_settings
+        text conversation_id
+        text created_at
+        text retweeted_tweet_id FK
+        text quoted_tweet_id FK
+        text replied_to_tweet_id FK
+        text in_reply_to_user_id FK
+        text author_id FK
+        text text
+        text lang
+        text source
+        integer possibly_sensitive
+        integer like_count
+        integer quote_count
+        integer reply_count
+        integer retweet_count
+        integer directly_collected
+    }
+    "_metadata" {
+        text metadata_key
+        text metadata_value
+    }
+    tweet |o--o{ tweet : "retweeted tweet"
+    tweet |o--o{ tweet : "quoted tweet"
+    tweet |o--o{ tweet : "replied to tweet"
+    tweet |o--o{ user : "in reply to user"
+    tweet |o--o{ user : "author"
+```
+
+Table **url**:
+- **source_id** (text): the id of the object (user or tweet) this URL is included in
+- **source_type** (text): "user" or "tweet"
+- **field** (text not null): e.g. "description", "text" - which field of the source object the URL is in
+- **url** (text not null): t.co shortened URL
+- **expanded_url** (text)
+- **display_url** (text)
+
+Table **hashtag**:
+- **source_id** (text): the id of the object (user or tweet) this hashtag is included in
+- **source_type** (text): "user" or "tweet"
+- **field** (text not null): e.g. "description", "text" - which field of the source object the hashtag is in
+- **tag** (text not null)
+
+Table **mention**:
+- **source_id** (text): the id of the object (user or tweet) this mention is included in
+- **source_type** (text): "user" or "tweet"
+- **field** (text not null): e.g. "description", "text" - which field of the source object the mention is in
+- **username** (text not null): username of mentioned user
+
+Table **media**:
+- **url** (text)
+- **preview_image_url** (text)
+- **height** (integer)
+- **width** (integer)
+- **type** (text)
+- **duration_ms** (integer)
+- **view_count** (integer)
+- **alt_text** (string)
+- **media_key** (text primary key)
+
+Table **user**:
+- **name** (text)
+- **profile_image_url** (text)
+- **id** (text primary key)
+- **created_at** (text)
+- **protected** (text)
+- **description** (text)
+- **location** (text)
+- **pinned_tweet_id** (text)
+- **verified** (integer): boolean
+- **url** (text)
+- **username** (text)
+
+Table **tweet**:
+- **id** (text primary key)
+- **reply_settings** (text)
+- **conversation_id** (text)
+- **created_at** (text)
+- **retweeted_tweet_id** (text references tweet (id))
+- **quoted_tweet_id** (text references tweet (id))
+- **replied_to_tweet_id** (text references tweet (id))
+- **in_reply_to_user_id** (text references user (id))
+- **author_id** (text references user (id))
+- **text** (text)
+- **lang** (text)
+- **source** (text)
+- **possibly_sensitive** (integer): boolean
+- **like_count** (integer)
+- **quote_count** (integer)
+- **reply_count** (integer)
+- **retweet_count** (integer)
+- **directly_collected** (integer): boolean
+
+Table **_metadata**:
+- **metadata_key** (text): primary key on conflict fail,
+- **metadata_value** (text)
+
